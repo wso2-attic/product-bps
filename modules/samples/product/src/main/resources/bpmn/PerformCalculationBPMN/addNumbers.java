ec83b7776247b1e46836bee8c5f7a337be887b5a
Himasha Guruge <himashag@wso2.com>
2015-05-12 13:58:23 +0530

    Refactor code for samples/tests

--- modules/samples/product/src/main/resources/bpmn/PerformCalculationBPMN/addNumbers.java
+++ modules/samples/product/src/main/resources/bpmn/PerformCalculationBPMN/addNumbers.java
@@ -2,7 +2,7 @@ package performCalculationBPMN;
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.JavaDelegate;
 
-public class addNumbers implements JavaDelegate{
+public class AddNumbers implements JavaDelegate{
     
 	@Override 
 	public void  execute(DelegateExecution execution) throws Exception {
